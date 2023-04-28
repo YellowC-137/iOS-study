3892337a30f25c8506d39bc4178fd4335c6b5f5c
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2023-04-28 01:27:13 -0700

    [chore] init

---
+++ basicstudy/basicstudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  basicstudy
+//
+//  Created by 맥 on 2023/04/28.
+//  Copyright © 2023 맥. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
