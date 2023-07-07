5197a1044a47b308ab9940fcfa65d74eea8ad3a2
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2023-07-07 13:58:06 +0900

    scrumding
    base set

---
+++ UsingStacksToArrangeViews/scrumding/scrumding/scrumdingApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingApp.swift
+//  scrumding
+//
+//  Created by 황준성 on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
