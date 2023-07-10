360e946822079b7a727b297bee50894e1e9e3713
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2023-07-10 16:47:35 +0900

    ch4

--- scrumding/scrumding/ContentView.swift
+++ scrumding/scrumding/ContentView.swift
@@ -10,11 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
             ProgressView(value: 5, total: 15)
+            //기본 정렬은 center 이고 leading,trailing으로 좌우 정렬
+            HStack{
+                VStack(alignment: .leading){
+                    Text("Elapsed")
+                    Label("3000",systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing)
+                {Text("Remained")
+                    Label("0",systemImage: "hourglass.bottomhalf.fill")
+                }
+                
+            }
+            Circle().strokeBorder(lineWidth: 24)
+            
+            HStack{
+                Text("1 / 3")
+                Spacer()
+                Button(action : {}){
+                    Image(systemName: "forward.fill")
+                }
+            }
+            
         }
         .padding()
     }
