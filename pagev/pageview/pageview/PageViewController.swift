c35a0f225f8dc31daa7130ecaeb5dac1370d7e8c
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2023-08-02 13:11:39 +0900

    edit
    ListCell nil error

--- pagev/pageview/pageview/PageViewController.swift
+++ pagev/pageview/pageview/PageViewController.swift
@@ -9,6 +9,18 @@ import Foundation
 import UIKit
 class PageViewController : UIPageViewController,UIPageViewControllerDelegate,UIPageViewControllerDataSource {
     
+    var currentIndex : Int?
+    var pendingIndex : Int?
+    
+    var isCheck:Bool = false{
+        didSet{
+            let imageName = isCheck ? "checkmark.square" : "checkmark.square.fill"
+            //
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         self.dataSource = self
         self.delegate = self
@@ -18,10 +30,34 @@ class PageViewController : UIPageViewController,UIPageViewControllerDelegate,UIP
         }
     }
 
+    lazy var indicator : UIPageControl = {
+        let pageControl = UIPageControl()
+        
+        pageControl.currentPageIndicatorTintColor = .orange
+        pageControl.pageIndicatorTintColor = .cyan
+        pageControl.numberOfPages = 3
+        pageControl.currentPage = 0
+        pageControl.preferredIndicatorImage = UIImage(named: "pageIndicator_default")
+         return pageControl
+    }()
+    
     
     lazy var vcArr : [UIViewController] = {
         return [self.vcInstance(name:"First" ),self.vcInstance(name: "Second"),self.vcInstance(name: "Third")]
     }()
+    
+    func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
+        pendingIndex = vcArr.firstIndex(of: pendingViewControllers.first!)
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            currentIndex = pendingIndex
+            if let index = currentIndex {
+                indicator.currentPage = index
+            }
+        }
+    }
     
     func vcInstance(name:String) -> UIViewController {
         return UIStoryboard(name:"Main",bundle: nil).instantiateViewController(withIdentifier: name)
