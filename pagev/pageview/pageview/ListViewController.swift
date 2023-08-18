b0ba62e536619b3822cd5bae23dc44c57c7170c8
YellowC-137 <55780312+YellowC-137@users.noreply.github.com>
2023-08-18 13:59:00 +0900

    clean

--- pagev/pageview/pageview/ListViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ListViewController.swift
-//  pageview
-//
-//  Created by 황준성 on 2023/08/02.
-//
-
-import Foundation
--- storyboard/storyboard/storyboard/AuthTable.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AuthTable.swift
-//  storyboard
-//
-//  Created by 황준성 on 2023/08/01.
-//
-
-import Foundation
